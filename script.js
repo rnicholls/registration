0c039acee90cb36cb0b9e3406c596864f0319c81
Ronald Nicholls <ronald@boxofficebuz.com>
2023-06-07 21:33:11 -0400

    changed lint indent back to 4

--- script.js
+++ script.js
@@ -1,84 +1,84 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const form = document.querySelector('#registration-form');
-  const nameInput = document.querySelector('#name');
-  const emailInput = document.querySelector('#email');
-  const passwordInput = document.querySelector('#password');
-  const confirmPasswordInput = document.querySelector('#confirm-password');
-  const thankYou = document.querySelector('#thank-you');
+    const form = document.querySelector('#registration-form');
+    const nameInput = document.querySelector('#name');
+    const emailInput = document.querySelector('#email');
+    const passwordInput = document.querySelector('#password');
+    const confirmPasswordInput = document.querySelector('#confirm-password');
+    const thankYou = document.querySelector('#thank-you');
 
-  form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    clearErrorMessages();
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        clearErrorMessages();
 
-    const name = nameInput.value.trim();
-    const email = emailInput.value.trim();
-    const password = passwordInput.value.trim();
-    const confirmPassword = confirmPasswordInput.value.trim();
+        const name = nameInput.value.trim();
+        const email = emailInput.value.trim();
+        const password = passwordInput.value.trim();
+        const confirmPassword = confirmPasswordInput.value.trim();
 
-    if (!validateName(name)) {
-      showError(nameInput, 'Please enter a valid name.');
+        if (!validateName(name)) {
+            showError(nameInput, 'Please enter a valid name.');
+        }
+
+        if (!validateEmail(email)) {
+            showError(emailInput, 'Please enter a valid email address.');
+        }
+
+        if (!validatePassword(password)) {
+            showError(passwordInput, 'Please enter a valid password (at least 8 characters, one uppercase, one lowercase, one digit).');
+        }
+
+        if (password !== confirmPassword) {
+            showError(confirmPasswordInput, 'Passwords do not match.');
+        }
+
+        if (!validateName(name) || !validateEmail(email) || !validatePassword(password) || password !== confirmPassword) {
+            return;
+        }
+
+        fetchpost(e);
+    });
+
+    function fetchpost(e) {
+        // (A) GET FORM DATA
+        const data = new FormData(e.currentTarget);
+
+        // (B) FETCH
+        fetch(e.currentTarget.action, { method: 'post', body: data })
+            .then(res => res.text())
+            .then(() => {
+                // do something when server responds
+                form.classList.add('hidden');
+                thankYou.classList.remove('hidden');
+            })
+            .catch(() => {});
+        // (C) PREVENT HTML FORM SUBMIT
+        return false;
     }
 
-    if (!validateEmail(email)) {
-      showError(emailInput, 'Please enter a valid email address.');
+    function validateName(name) {
+        return /^[a-zA-Z]+$/.test(name);
     }
 
-    if (!validatePassword(password)) {
-      showError(passwordInput, 'Please enter a valid password (at least 8 characters, one uppercase, one lowercase, one digit).');
+    function validateEmail(email) {
+        // Basic email validation, can be improved
+        return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
     }
 
-    if (password !== confirmPassword) {
-      showError(confirmPasswordInput, 'Passwords do not match.');
+    function validatePassword(password) {
+        // Password must contain at least 8 characters, one uppercase, one lowercase, one digit
+        return /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/.test(password);
     }
 
-    if (!validateName(name) || !validateEmail(email) || !validatePassword(password) || password !== confirmPassword) {
-      return;
+    function showError(input, message) {
+        const formGroup = input.parentElement;
+        const errorMessage = document.createElement('div');
+        errorMessage.className = 'error-message';
+        errorMessage.innerText = message;
+        formGroup.appendChild(errorMessage);
+        input.classList.add('error');
     }
 
-    fetchpost(e);
-  });
-
-  function fetchpost(e) {
-    // (A) GET FORM DATA
-    const data = new FormData(e.currentTarget);
-
-    // (B) FETCH
-    fetch(e.currentTarget.action, { method: 'post', body: data })
-      .then(res => res.text())
-      .then(() => {
-        // do something when server responds
-        form.classList.add('hidden');
-        thankYou.classList.remove('hidden');
-      })
-      .catch(() => {});
-    // (C) PREVENT HTML FORM SUBMIT
-    return false;
-  }
-
-  function validateName(name) {
-    return /^[a-zA-Z]+$/.test(name);
-  }
-
-  function validateEmail(email) {
-    // Basic email validation, can be improved
-    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
-  }
-
-  function validatePassword(password) {
-    // Password must contain at least 8 characters, one uppercase, one lowercase, one digit
-    return /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/.test(password);
-  }
-
-  function showError(input, message) {
-    const formGroup = input.parentElement;
-    const errorMessage = document.createElement('div');
-    errorMessage.className = 'error-message';
-    errorMessage.innerText = message;
-    formGroup.appendChild(errorMessage);
-    input.classList.add('error');
-  }
-
-  function clearErrorMessages() {
-    document.querySelectorAll('.error-message').forEach(e => e.remove());
-  }
+    function clearErrorMessages() {
+        document.querySelectorAll('.error-message').forEach(e => e.remove());
+    }
 });
--- server.js
+++ server.js
@@ -3,21 +3,21 @@ const fs = require('fs');
 const path = require('path');
 
 const server = http.createServer((req, res) => {
-  let filePath = req.url === '/' ? '/index.html' : req.url;
-  const mimeType = req.url.match('.css$') ? 'text/css' : 'text/html';
+    let filePath = req.url === '/' ? '/index.html' : req.url;
+    const mimeType = req.url.match('.css$') ? 'text/css' : 'text/html';
 
-  filePath = path.join(__dirname, filePath);
+    filePath = path.join(__dirname, filePath);
 
-  fs.readFile(filePath, 'utf-8', (err, content) => {
-    if (err) {
-      res.writeHead(404);
-      res.end('File not found!');
-      return;
-    }
+    fs.readFile(filePath, 'utf-8', (err, content) => {
+        if (err) {
+            res.writeHead(404);
+            res.end('File not found!');
+            return;
+        }
 
-    res.writeHead(200, { 'Content-Type': mimeType });
-    res.end(content);
-  });
+        res.writeHead(200, { 'Content-Type': mimeType });
+        res.end(content);
+    });
 });
 
 const port = 3000;
